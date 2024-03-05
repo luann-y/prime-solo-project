247d62fb5794ad4c24679ea158efc1c77b3eb873
LuAnn Yang <luann.yang@gmail.com>
2024-03-04 19:48:38 -0600

    importing Google font link for font use and adding css to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ function Header() {
     return(
         <div className="hero">
             <img src={myImage} alt="Coffee Image" />
+            <h1 className="montserrat-bold">Coffee Connection</h1>
         </div>
     );
 };
